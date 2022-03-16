335ea36be6a42c26dd76bdd7861d83e377a00f7c
Timothy Hart <timothyhart6@gmail.com>
2022-03-16 14:09:37 -0400

    Home screen address submission redirects to userDistrict page and displays entered data

--- src/main/java/com/civilservants/controller/UserDistrictController.java
+++ src/main/java/com/civilservants/controller/UserDistrictController.java
@@ -10,9 +10,10 @@ public class UserDistrictController {
 
     @GetMapping("/userDistrict")
 //    public String displayUserDistrict(@ModelAttribute("user") User user)
-    public String displayUserDistrict(@RequestParam String address, Model model)
+    public String displayUserDistrict(@RequestParam String streetAddress, String zipCode, Model model)
     {
-        model.addAttribute("address", address);
+        model.addAttribute("streetAddress", streetAddress);
+        model.addAttribute("zipCode", zipCode);
         //Maybe this is how to get all the reps. getReps will call all the apis and create all the objects needed. It will return list of reps.
 //        model.addAttribute("userReps", UserReps.getReps(address));
         return "userDistrict";
