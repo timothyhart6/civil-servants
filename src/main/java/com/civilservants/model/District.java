ef5c3ed5fbc3344f1c668fb91bdd186dd2140d3d
Timothy Hart <timothyhart6@gmail.com>
2022-04-18 17:17:21 -0400

    creating propublica house members object

--- src/main/java/com/civilservants/model/District.java
+++ src/main/java/com/civilservants/model/District.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 public class District {
 
     private AddressAndDistrictInfo addressAndDistrictInfo;
+//    private Senator senatorOne;
     private HouseMember houseMember;
 
     public District(String streetAddress, String zipCode){
@@ -15,6 +16,9 @@ public class District {
     }
 
     public void buildDistrict(String streetAddress, String zipCode){
+//        ArrayList<T> apiObjects = ApiCaller.makeApiCallsForDistrict(streetAddress, zipCode);
+//        this.senatorOne = RepresentativeBuilder.buildSenator(apiObjects);
+//        this.houseMember = RepresentativeBuilder.buildHouseMember(apiObjects);
         GoogleApiCalls googleApiCalls = new GoogleApiCalls();
         ArrayList<String> googleAddressAndDistrictInfo = googleApiCalls.fetchAddressAndDistrictInfo(streetAddress, zipCode);
         this.addressAndDistrictInfo = new AddressAndDistrictInfo(streetAddress, googleAddressAndDistrictInfo.get(1), googleAddressAndDistrictInfo.get(2), zipCode, googleAddressAndDistrictInfo.get(4));
---
+++ src/main/java/com/civilservants/model/api/proPublica/HouseMembers.java
@@ -0,0 +1,34 @@
+package com.civilservants.model.api.proPublica;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HouseMembers {
+    private String status;
+    private String copyright;
+    private List<HouseMember> results = new ArrayList<>();
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public List<HouseMember> getResults() {
+        return results;
+    }
+
+    public void setResults(List<HouseMember> results) {
+        this.results = results;
+    }
+}
