7465a7f278bf92f90368fd7406c583cf5ab1090f
Timothy Hart <timothyhart6@gmail.com>
2022-05-19 15:43:12 -0400

    Removing all logic from District and moving it to the associated classes. Separating fetchDistrict logic into separate class from UserAddress

--- src/main/java/com/civilservants/service/ProPublicaApiCalls.java
+++ src/main/java/com/civilservants/service/ProPublicaApiCalls.java
@@ -11,18 +11,13 @@ import org.springframework.web.client.RestTemplate;
 
 public class ProPublicaApiCalls {
 
-    private static final String PROPUBLICA_API = APIKeyStore.getKey("PROPUBLICA_API");
-
-    public void FetchHouseMember(String address, String zipCode){
-//        PropublicaHouseMembers propublicaHouseMembers = getPropublicaApiRepresentativesModel(address, zipCode);
-
-    }
+    private static final String PROPUBLICA_API_KEY = APIKeyStore.getKey("PROPUBLICA_API");
 
     public ProPublicaHouseMember getPropublicaApiRepresentativesModel(String state, String districtCode) {
         String proPublicaRepresentativesUrl = "https://api.propublica.org/congress/v1/members/house/" + state + "/" + districtCode + "/current.json";
         HttpHeaders headers = new HttpHeaders();
-        headers.set("X-API-Key", PROPUBLICA_API);
-        final HttpEntity<String> entity = new HttpEntity<String>(headers);
+        headers.set("X-API-Key", PROPUBLICA_API_KEY);
+        final HttpEntity<String> entity = new HttpEntity<>(headers);
         RestTemplate restTemplate = new RestTemplate();
 
         ResponseEntity<PropublicaHouseMembers> response = restTemplate.exchange(proPublicaRepresentativesUrl, HttpMethod.GET, entity, PropublicaHouseMembers.class);
